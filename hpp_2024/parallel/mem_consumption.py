1dab4b18b77bb5a92f9acbf99112a9160a80d45c
Piotr Marecki <pmarecki@gmail.com>
2024-10-26 10:49:09 +0200

    measure time of memory allocation

--- hpp_2024/parallel/mem_consumption.py
+++ hpp_2024/parallel/mem_consumption.py
@@ -1,10 +1,15 @@
 import tracemalloc
 
+from hpp_2024.parallel.utils import ts
+
 if __name__ == '__main__':
+    st = ts()
     tracemalloc.start()
     mem_current, mem_peak = tracemalloc.get_traced_memory()
     print(f'{mem_current=}, {mem_peak=}')
     f = [0.0 for i in range(10 ** 8)]
     mem_current, mem_peak = tracemalloc.get_traced_memory()
-    print(f'{mem_current=}, {mem_peak=}')
+    print(f'{mem_current=}, mem_peak={mem_peak // (1024**2)} MB')
+    en = ts()
+    print(f'memory allocated in {en-st:.3f}s')
     s = input('end? ')
